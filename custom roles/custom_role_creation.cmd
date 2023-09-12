32d8a54435948b97f66d581431954a129f17fffd
arunkkumar1 <arun.k.kumar@gmail.com>
2023-09-12 15:42:16 -0400

    Updated command to create custom role

--- custom roles/custom_role_creation.cmd
+++ custom roles/custom_role_creation.cmd
@@ -1,12 +1,24 @@
 //Custom role creation commands
 
+// Reference documentation: 
+// https://learn.microsoft.com/en-us/azure/role-based-access-control/custom-roles-cli
+
+// Permissions available for each resource provider:
+// https://learn.microsoft.com/en-us/azure/role-based-access-control/resource-provider-operations
+
 //Create role
 az role definition create --role-definition CustomRole-Cognitive-Services-OpenAI-User.json
 az role definition create --role-definition CustomRole-SupportEngg-AOAI.json
 
 //Update role
-az role definition update --role-definition CustomRole-Cognitive-Services-OpenAI-User.json
-az role definition update --role-definition CustomRole-SupportEngg-AOAI.json
+//generate existing definition and save to json file (remove square brackets from saved file)
+az role definition list --name CustomRole-Cognitive-Services-OpenAI-User.json > CustomRole-Cognitive-Services-OpenAI-User-update.json
+
+
+
+//add new permissions to the json file and then execute the update command
+az role definition update --role-definition CustomRole-Cognitive-Services-OpenAI-User-update.json
+
 
 //Delete role
 az role definition delete --name <CustomRoleName>
